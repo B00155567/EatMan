03904346e17ad45598dc0a19caf8f352dd70641d
nikiandy <B00152278@mytudublin.ie>
2023-11-15 06:00:40 +0000

    PauseMenu
    Finished full functionality of Start Menu
    and Pause Menu.
    Made Script for PauseMenu.

--- Assets/MainMenu.cs
+++ Assets/MainMenu.cs
@@ -4,8 +4,22 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour {
+
+    public string URL = "https://github.com/B00155567/EatMan";
     
     public void PlayGame() {
+        
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
+
+    public void loadUrl() {
+
+        Application.OpenURL(URL);
+    }
+    public void ExitGame() {
+        
+        Debug.Log("Exiting Game!");
+        Application.Quit();
+    }
+
 }
