441d9aaee3bf0ba5ff94e092720a03bd32289fbf
nikiandy <B00152278@mytudublin.ie>
2023-11-23 17:12:16 +0000

    Added more functionality to the Health Bar
    Green Yellow Orange red depending on the value of variable health.

--- Assets/HealthBar.cs
+++ Assets/HealthBar.cs
@@ -7,15 +7,20 @@ public class HealthBar : MonoBehaviour
 {
     public Slider hSlider;
     public Gradient hGradient;
+    public Image fill;
 
     public void SetMaxHealth(int health) {
 
         hSlider.maxValue = health;
         hSlider.value = health;
+
+        fill.color = hGradient.Evaluate(1f);
     }
 
     public void SetHealth(int health) {
 
         hSlider.value = health;
+
+        fill.color = hGradient.Evaluate(hSlider.normalizedValue);
     }
 }
