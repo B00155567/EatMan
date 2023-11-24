8de1a11c5a7eabfa2519c572a70165f46995c472
B00155567 <b00155567@mytudublin.ie>
2023-11-24 22:17:22 +0000

    Zombie Spawner

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -2,17 +2,25 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ZombieSpawner : MonoBehaviour
+public class EnemySpawner : MonoBehaviour
 {
+    [SerializeField]
+    private GameObject zombiePrefab;
+
+    [SerializeField]
+    private float zombieInterval = 3.5f;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        StartCoroutine(spawnEnemy(zombieInterval, zombiePrefab));
     }
 
-    // Update is called once per frame
-    void Update()
+    private IEnumerator spawnEnemy(float interval, GameObject enemy)
     {
-        
-    }
+        yield return new WaitForSeconds(interval);
+        GameObject newEnemy = Instantiate(enemy, new Vector3(Random.Range(-20f, 20), Random.Range(-6f, 1f), 0), Quaternion.identity);
+        StartCoroutine(spawnEnemy(interval, enemy));
+    } 
 }
