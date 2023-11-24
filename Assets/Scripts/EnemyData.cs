228add3438fb44cec760f37a4d671931446e7990
B00155567 <b00155567@mytudublin.ie>
2023-11-24 19:33:25 +0000

    enemy logic 2

--- Assets/Scripts/EnemyData.cs
+++ Assets/Scripts/EnemyData.cs
@@ -2,11 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[CreateAssetMenu(fileName = "EnemyData", menuName = "EnemyData", order = 1)]
-public class EnemyData : ScriptableObject 
+[CreateAssetMenu(fileName = "Data", menuName = "ScriptableObjects/Enemy", order = 1)]
+public class EnemyData : ScriptableObject
 {
-    public int health;
-    public int speed;
+    public int hp;
     public int damage;
-
+    public float speed;
 }
